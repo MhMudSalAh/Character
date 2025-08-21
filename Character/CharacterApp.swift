8d458927e56f6884828bb4f44f9c654ce5c04f1e
MhMud-SalAh <mhmud.salah.hamed@gmail.com>
2025-08-22 02:17:37 +0300

    Initial Commit

---
+++ Character/CharacterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterApp.swift
+//  Character
+//
+//  Created by MhMuD SalAh on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CharacterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
